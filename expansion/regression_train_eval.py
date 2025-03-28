2d7b0b293f9bd0836a35fac71db611f45c0996e8
tgebhart <gebharttom8@gmail.com>
2025-03-28 09:22:29 -0500

    add model save funcationlity

--- expansion/regression_train_eval.py
+++ expansion/regression_train_eval.py
@@ -1,4 +1,6 @@
+import os
 import json
+import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import AutoTokenizer, AutoModel
@@ -47,37 +49,57 @@ def embed_abstracts(abstracts, model, tokenizer, device):
     embeddings = outputs.last_hidden_state[:, 0, :]
     return embeddings
 
-def train_and_evaluate(batch_size, train_file, test_file, label_key, save_file):
+def train_and_evaluate(batch_size, train_file, test_file, label_key, save_loc, train=True):
+    result_save_file = os.path.join(save_loc, f"{label_key}_outputs.jsonl")
+    model_save_file = os.path.join(save_loc, f"{label_key}_model.pth")
+    loss_save_file = os.path.join(save_loc, f"{label_key}_losses.csv")
+
+    model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased').to(device)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
-    model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased').to(device)
+
+    if os.path.exists(model_save_file):
+        print(f"Model already exists at {model_save_file}. Loading...")
+        regressor = torch.load(model_save_file).to(device)
+    else:
+        regressor = Regressor(model.config.hidden_size).to(device)
     
-    train_dataset = JsonlDataset(train_file, label_key)
-    test_dataset = JsonlDataset(test_file, label_key)
-    
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False)
-    
-    regressor = Regressor(model.config.hidden_size).to(device)
     criterion = torch.nn.MSELoss()
-    
     optimizer = torch.optim.SGD(regressor.parameters(), lr=0.01, weight_decay=0.0001)
     
-    # Training
-    regressor.train()
-    pbar = tqdm(train_loader)
-    for abstracts, labels in pbar:
-        embeddings = embed_abstracts(abstracts, model, tokenizer, device).to(device)
-        labels = labels.to(device)
+    if train:
+        # Training
+        train_dataset = JsonlDataset(train_file, label_key)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
         
-        optimizer.zero_grad()
-        outputs = regressor(embeddings).squeeze()
-        loss = criterion(outputs, labels)
-        pbar.set_description(f"Training (loss: {loss.item():.4f})")
-        loss.backward()
-        optimizer.step()
+        losses = np.array([])
+        if os.path.exists(loss_save_file):
+            losses = np.loadtxt(loss_save_file, delimiter=',')
+
+        regressor.train()
+        pbar = tqdm(train_loader)
+        for abstracts, labels in pbar:
+            embeddings = embed_abstracts(abstracts, model, tokenizer, device).to(device)
+            labels = labels.to(device)
+            
+            optimizer.zero_grad()
+            outputs = regressor(embeddings).squeeze()
+            loss = criterion(outputs, labels)
+            losses = np.append(losses, loss.item())
+            pbar.set_description(f"Training (loss: {loss.item():.4f})")
+            loss.backward()
+            optimizer.step()
         
+        # Save the trained model
+        torch.save(regressor.state_dict(), model_save_file)
+        np.savetxt(loss_save_file, losses, delimiter=',')
+        print(f"Model saved to {model_save_file}")
+        print(f"Losses saved to {loss_save_file}")
+
     # Evaluation
+    test_dataset = JsonlDataset(test_file, label_key)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False)
+
     regressor.eval()
     all_preds = []
     with torch.no_grad():
@@ -87,15 +109,16 @@ def train_and_evaluate(batch_size, train_file, test_file, label_key, save_file):
             outputs = regressor(embeddings).squeeze()
             all_preds.extend(outputs.cpu().numpy())
 
-    with open(save_file, 'w') as f:
+    with open(result_save_file, 'w') as f:
         for i in tqdm(range(len(all_preds)), desc="Saving predictions"):
             pred = all_preds[i]
             f.write(json.dumps({'index':i, 'prediction': pred}) + '\n')
 
 if __name__ == "__main__":
-    batch_size = 128
+    batch_size = 1024
     train_file = 'data/s2ag/expansion/paper_regressions/train.jsonl'
     test_file = 'data/s2ag/expansion/paper_regressions/test.jsonl'
     label_key = 'year'  # or 'i_5'
-    save_file = f'data/s2ag/expansion/paper_regressions/{label_key}_outputs.jsonl'
-    train_and_evaluate(batch_size, train_file, test_file, label_key, save_file)
+    save_loc = f'data/s2ag/expansion/paper_regressions'
+    train = True
+    train_and_evaluate(batch_size, train_file, test_file, label_key, save_loc, train=train)
